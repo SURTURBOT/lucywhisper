02a565290381793437208c448d1aa899224bbb95
Himanshu <96481899+himanshu-op@users.noreply.github.com>
2021-12-31 21:06:58 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -2,59 +2,24 @@ from pyrogram.types import InlineKeyboardButton
 
 
 class Data:
-    # Start Message
     START = """
-Hey {}.
-Welcome to {}
-
-I am the Master of Whisperers (like Varys in Game of Thrones).
-
-You can use me to send whispers to your friend in groups and channels (even if I'm not there).
-Only that friend and you will be able to read the message even though others are in same group. 
-
-To see how to use me press 'How to Use' below.
-
-By @StarkBots
+Hello , I am Whisper Bot and I can help you in sending secret messages in groups.
     """
 
     # Home Button
     home_buttons = [
-        [InlineKeyboardButton("🔒 Send a Whisper 🔒", switch_inline_query="")],
-        [InlineKeyboardButton(text="🏠 Return Home 🏠", callback_data="home")],
-    ]
+        [InlineKeyboardButton("📬 Send a secret Message 📬", switch_inline_query="")],
+        ]
     # Rest Buttons
     buttons = [
         [
-            InlineKeyboardButton("🔒 Send a Whisper 🔒", switch_inline_query="")
+            InlineKeyboardButton("🔒 Send a Secret Message 🔒", switch_inline_query="")
         ],
         [
-            InlineKeyboardButton("How to Use ❔", callback_data="help"),
-            InlineKeyboardButton("🎪 About 🎪", callback_data="about")
+            InlineKeyboardButton("How to Use Me❔", url="https://teletype.in/@wetemp/wisper"),
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/StarkBots")],
-        [InlineKeyboardButton("🎨 Support Group 🎨", url="https://t.me/StarkBotsChat")],
+        [InlineKeyboardButton("⚡Bot 🇽  Network⚡", url="https://t.me/BotxNetwork")],
+        [InlineKeyboardButton("🛠️ Support Group 🛠️", url="https://t.me/BotxDesk")],
     ]
 
-    # Help Message
-    HELP = """
-Just type the message in below format in any chat.
-
-`@WhisperStarkBot your_message friend_username/id`
-    """
-
-    # About Message
-    ABOUT = """
-**About This Bot** 
-
-Bot created by @StarkBots
-
-Source Code : [Click Here](https://github.com/StarkBotsIndustries/WhisperBot)
-
-Inspired By : nnbbot
-
-Framework : [Pyrogram](docs.pyrogram.org)
-
-Language : [Python](www.python.org)
-
-Developer : @StarkProgrammer
-    """
+  
